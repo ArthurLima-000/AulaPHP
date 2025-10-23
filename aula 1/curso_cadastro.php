3dfe9e6b66ce34bfd769d0c7ee33ceaf73c100bd
Arthur Lima <aarthurxx@gmail.com>
2025-10-23 14:49:12 -0300

    23/10

---
+++ aula 1/curso_cadastro.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro Cursos</title>
+</head>
+<body>
+    <h1>Cadastro de Cursos </h1>
+
+    <form>
+        <p>Informe o nome do curso:</p>
+        <input type="text" name="curso">
+
+        <p>Informe o nome do coordenador:</p>
+        <input type="text" name="coordenador"><br>
+
+        <input type="submit" value="Cadastrar Curso">
+    </form>
+</body>
+</html>
---
+++ aula 1/professor_cadastro.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de Professores</title>
+</head>
+<body>
+    <h1>Cadastro de Professores</h1>
+
+    <form>
+        <p>Informe o nome do professor:</p>
+        <input type="text" name="professor">
+
+        <input type="submit" value="Cadastrar Professor">
+
+    </form>
+</body>
+</html>
